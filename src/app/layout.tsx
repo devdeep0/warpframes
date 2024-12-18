16452e51da805b01452fe84cda21f9ffb5c5fb7c
devdeep0 <deepanshuvalia740@gmail.com>
2024-12-19 01:48:17 +0530

    fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import "~/app/globals.css";
 import  Providers  from "~/app/providers";
 export const metadata: Metadata = {
-  title: "Farcaster Frames v2 Demo",
-  description: "A Farcaster Frames v2 demo app",
+  title: "Rupture Labs Farcaster",
+  description: "games by rupture labs",
 };
 
 export default function RootLayout({
